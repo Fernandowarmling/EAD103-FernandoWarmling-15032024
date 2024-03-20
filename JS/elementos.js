dd6ad1f2173e05181a5e2791947807bb5b6d8c63
fernandowarmling <warmlingfernando@gmail.com>
2024-03-19 21:01:18 -0300

    Posição 18/03/2024

---
+++ JS/elementos.js
@@ -0,0 +1,6 @@
+$(function () {
+    $('#header').load("../HTML/header.html");
+    $('#nav').load("../HTML/nav.html")
+    $('#section').load("../HTML/section.html");
+    $('#footer').load("../HTML/footer.html");
+});
